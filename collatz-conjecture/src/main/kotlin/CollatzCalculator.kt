b6a9e49b248ec4afb3133cf3120ebf38783b523b
Uzi Landsmann <uzi.landsmann@bisnode.com>
2019-04-13 10:40:58 +0200

    Updated kotlin/collatz conjecture

--- collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -1,12 +1,8 @@
 object CollatzCalculator {
-    fun computeStepCount(n: Int): Int {
+    fun computeStepCount(n: Int, counter: Int = 0): Int {
         require(n > 0) { "Only natural numbers are allowed" }
-        return helper(n, 0)
-    }
-
-    private fun helper(n: Int, counter: Int): Int {
         if (n == 1) return counter
         val temp = if (n.rem(2) == 0) n / 2 else n * 3 + 1
-        return helper(temp, counter + 1)
+        return computeStepCount(temp, counter + 1)
     }
 }
