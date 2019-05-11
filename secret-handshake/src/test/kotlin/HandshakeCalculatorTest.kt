9576c8eec15dc86197c8d059763f4ce6634d896f
Uzi Landsmann <uzi.landsmann@bisnode.com>
2019-05-11 09:07:02 +0200

    Updated kotlin/secret handshake

--- secret-handshake/src/test/kotlin/HandshakeCalculatorTest.kt
+++ secret-handshake/src/test/kotlin/HandshakeCalculatorTest.kt
@@ -1,3 +1,4 @@
+
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -6,84 +7,96 @@ class HandshakeCalculatorTest {
     @Test
     fun testThatInput1YieldsAWink() {
         assertEquals(
-                listOf(Signal.WINK),
-                HandshakeCalculator.calculateHandshake(1))
+            listOf(Signal.WINK),
+            HandshakeCalculator.calculateHandshake(1))
     }
 
+    
     @Test
     fun testThatInput2YieldsADoubleBlink() {
         assertEquals(
-                listOf(Signal.DOUBLE_BLINK),
-                HandshakeCalculator.calculateHandshake(2))
+            listOf(Signal.DOUBLE_BLINK),
+            HandshakeCalculator.calculateHandshake(2))
     }
 
+    
     @Test
     fun testThatInput4YieldsACloseYourEyes() {
         assertEquals(
-                listOf(Signal.CLOSE_YOUR_EYES),
-                HandshakeCalculator.calculateHandshake(4))
+            listOf(Signal.CLOSE_YOUR_EYES),
+            HandshakeCalculator.calculateHandshake(4))
     }
 
+    
     @Test
     fun testThatInput8YieldsAJump() {
         assertEquals(
-                listOf(Signal.JUMP),
-                HandshakeCalculator.calculateHandshake(8))
+            listOf(Signal.JUMP),
+            HandshakeCalculator.calculateHandshake(8))
     }
 
+    
     @Test
     fun testAnInputThatYieldsTwoActions() {
         assertEquals(
-                listOf(Signal.WINK, Signal.DOUBLE_BLINK),
-                HandshakeCalculator.calculateHandshake(3))
+            listOf(Signal.WINK, Signal.DOUBLE_BLINK),
+            HandshakeCalculator.calculateHandshake(3))
     }
 
+    
     @Test
     fun testAnInputThatYieldsTwoReversedActions() {
         assertEquals(
-                listOf(Signal.DOUBLE_BLINK, Signal.WINK),
-                HandshakeCalculator.calculateHandshake(19))
+            listOf(Signal.DOUBLE_BLINK, Signal.WINK),
+            HandshakeCalculator.calculateHandshake(19))
     }
 
+    
     @Test
     fun testReversingASingleActionYieldsTheSameAction() {
         assertEquals(
-                listOf(Signal.JUMP),
-                HandshakeCalculator.calculateHandshake(24))
+            listOf(Signal.JUMP),
+            HandshakeCalculator.calculateHandshake(24))
     }
 
+    
     @Test
     fun testReversingNoActionsYieldsNoActions() {
         assertEquals(
-                emptyList(),
-                HandshakeCalculator.calculateHandshake(16))
+            emptyList(),
+            HandshakeCalculator.calculateHandshake(16))
     }
 
+    
     @Test
     fun testInputThatYieldsAllActions() {
         assertEquals(
-                listOf(Signal.WINK, Signal.DOUBLE_BLINK, Signal.CLOSE_YOUR_EYES, Signal.JUMP),
-                HandshakeCalculator.calculateHandshake(15))
+            listOf(Signal.WINK, Signal.DOUBLE_BLINK, Signal.CLOSE_YOUR_EYES, Signal.JUMP),
+            HandshakeCalculator.calculateHandshake(15))
     }
 
+    
     @Test
     fun testInputThatYieldsAllActionsReversed() {
         assertEquals(
-                listOf(Signal.JUMP, Signal.CLOSE_YOUR_EYES, Signal.DOUBLE_BLINK, Signal.WINK),
-                HandshakeCalculator.calculateHandshake(31))
+            listOf(Signal.JUMP, Signal.CLOSE_YOUR_EYES, Signal.DOUBLE_BLINK, Signal.WINK),
+            HandshakeCalculator.calculateHandshake(31))
     }
 
+    
     @Test
     fun testThatInput0YieldsNoActions() {
         assertEquals(
-                emptyList(),
-                HandshakeCalculator.calculateHandshake(0))
+            emptyList(),
+            HandshakeCalculator.calculateHandshake(0))
     }
 
+    
     @Test
     fun testThatInputWithLower5BitsNotSetYieldsNoActions() {
         assertEquals(
-                emptyList(),
-                HandshakeCalculator.calculateHandshake(32))
+            emptyList(),
+            HandshakeCalculator.calculateHandshake(32))
     }
+
 }
