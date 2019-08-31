41662e876532ac777fb494f670bd129bd098085f
Uzi Landsmann <uzi.landsmann@bisnode.com>
2019-08-31 13:45:03 +0200

    Updated kotlin/secret handshake

--- secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,11 +1,9 @@
 object HandshakeCalculator {
+    private const val REVERSED = 0b10000
+
     fun calculateHandshake(num: Int): List<Signal> {
-        val arr = ArrayList<Signal>()
-        if (num and 0b1 == 0b1) arr.add(Signal.WINK)
-        if (num and 0b10 == 0b10) arr.add(Signal.DOUBLE_BLINK)
-        if (num and 0b100 == 0b100) arr.add(Signal.CLOSE_YOUR_EYES)
-        if (num and 0b1000 == 0b1000) arr.add(Signal.JUMP)
-        if (num and 0b10000 == 0b10000) arr.reverse()
-        return arr
-    }   
+        return Signal.values()
+                .filter { num and it.value == it.value }
+                .apply { return if (num and REVERSED == REVERSED) reversed() else this }
+    }
 }
--- secret-handshake/src/main/kotlin/Signal.kt
+++ secret-handshake/src/main/kotlin/Signal.kt
@@ -1,5 +1,6 @@
-enum class Signal {
-
-  WINK, DOUBLE_BLINK, CLOSE_YOUR_EYES, JUMP
-
+enum class Signal(val value: Int) {
+    WINK(0b1),
+    DOUBLE_BLINK(0b10),
+    CLOSE_YOUR_EYES(0b100),
+    JUMP(0b1000);
 }
