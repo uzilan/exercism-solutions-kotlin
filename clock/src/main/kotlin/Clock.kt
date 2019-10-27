661590ef6f952c0f8314717ceb1559b10bafe030
Uzi Landsmann <uzi.landsmann@bisnode.com>
2019-10-27 08:19:18 +0100

    added kotlin/clock

---
+++ clock/src/main/kotlin/Clock.kt
@@ -0,0 +1,51 @@
+data class Clock(var hours: Int, var minutes: Int) {
+
+    init {
+        adjustTime()
+    }
+
+    override fun toString(): String {
+
+        val hh = when {
+            hours == 0 -> "00"
+            hours < 10 -> "0$hours"
+            else -> "$hours"
+        }
+
+        val mm = when {
+            minutes == 0 -> "00"
+            minutes < 10 -> "0$minutes"
+            else -> "$minutes"
+        }
+
+        return "$hh:$mm"
+    }
+
+    fun add(m: Int) {
+        this.minutes = minutes + m
+        adjustTime()
+    }
+
+    private fun adjustTime() {
+        if (minutes >= 60) {
+            hours += minutes / 60
+            minutes %= 60
+        }
+
+        while (minutes < 0) {
+            hours--
+            minutes += 60
+        }
+
+        if (hours >= 24) {
+            hours %= 24
+        }
+
+        while (hours < 0) {
+            hours += 24
+        }
+    }
+}
+
+
+
